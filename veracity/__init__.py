ba2b7a2cdec87cdce26fd0cbe3335378e1135a86
birb007 <25516647+birb007@users.noreply.github.com>
2020-04-16 22:35:52 +0100

    export functions

--- veracity/__init__.py
+++ veracity/__init__.py
@@ -1,2 +1,2 @@
-from .veracity import Parser, Variable, Conjunction, Disjunction, Implication, Negation
+from .veracity import Parser, Variable, Conjunction, Disjunction, Implication, Negation, stringify, solve, simplify
 __version__ = '0.1.0'
